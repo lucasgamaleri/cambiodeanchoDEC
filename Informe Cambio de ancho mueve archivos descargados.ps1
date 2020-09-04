f37fb0dd3284e415975455494fd51b745d9d7090
Lucas Axel Gamaleri <lucas.gamaleri.92@gmail.com>
2020-09-04 09:23:10 -0300

    First Commit - Script file

---
+++ Informe Cambio de ancho mueve archivos descargados.ps1
@@ -0,0 +1,4 @@
+﻿cd C:\Users\LGAMALER\Downloads
+Move-Item 'SGL_Demoras_Decapado_EDA.xlsx' 'C:\Users\LGAMALER\OneDrive - TERNIUM\Documentos\Decapado\2019.07 - Proyecto Cambios de Ancho\Informe diario'
+Move-Item 'SGL_Produccion_DECAPADO_EDA_Decapado_EDA.xlsx' 'C:\Users\LGAMALER\OneDrive - TERNIUM\Documentos\Decapado\2019.07 - Proyecto Cambios de Ancho\Informe diario'
+exit
